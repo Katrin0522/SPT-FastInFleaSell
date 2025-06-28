b72de0d38b52e53cb5149189945e20fd5b2720aa
Kat <egorsimonoff2018@gmail.com>
2025-06-29 02:01:05 +0300

    Init commit

---
+++ FastSoldInFleaPlugin.cs
@@ -0,0 +1,55 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using EFT.Communications;
+using EFT.InventoryLogic;
+using FastSoldInFlea.Patches;
+using SPT.Reflection.Utils;
+using UnityEngine;
+using FleaRequirement = GClass2102;
+
+namespace FastSoldInFlea
+{
+    [BepInPlugin("katrin0522.FastSoldInFlea", "Kat.FastSoldInFlea", "1.0.0")]
+    [BepInDependency("com.kmyuhkyuk.KmyTarkovApi", "1.4.0")]
+    public class FastSoldInFleaPlugin : BaseUnityPlugin
+    {
+        public static ManualLogSource logSource;
+
+        private ConfigEntry<KeyboardShortcut> keyBind;
+        public static ISession Session => ClientAppUtils.GetMainApp().GetClientBackEndSession();
+        public static bool IsKeyPressed;
+        public static string LastCatchedItemID;
+        public static double LastCatchedPrice;
+        public static Item LastCachedItem;
+
+        private void Update()
+        {
+            IsKeyPressed = keyBind.Value.IsPressed();
+        }
+        
+        private void Awake()
+        {
+            keyBind = Config.Bind("1. Settings", "KeyBindSell", new KeyboardShortcut(KeyCode.LeftShift)); 
+            
+            logSource = Logger;
+            logSource.LogInfo("FastSoldInFlea successful loaded!");
+
+            new FleaCatchPricePatch().Enable();
+            new FleaCatchItemPatch().Enable();
+        }
+        
+        public static void TryAddOfferToFlea(Item item, double unadjustedPrice)
+        {
+            var g = new FleaRequirement()
+            {
+                count = unadjustedPrice - 1,
+                _tpl = "5449016a4bdc2d6f028b456f"
+            };
+            
+            FleaRequirement[] gs = new FleaRequirement[1] { g };
+            Session.RagFair.AddOffer(false, new string[1] { item.Id }, gs, null);
+            NotificationManagerClass.DisplayMessageNotification($"Продаём оффер за {g.count}RUB", ENotificationDurationType.Default, ENotificationIconType.EntryPoint);
+        }
+    }
+}
---
+++ Patches/FleaCatchPricePatch.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using EFT.UI.DragAndDrop;
+using EFT.UI.Ragfair;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+
+namespace FastSoldInFlea.Patches
+{
+    internal class FleaCatchPricePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(ItemMarketPricesPanel), "method_1", null, null);
+        }
+
+        [PatchPrefix]
+        static bool Prefix(ItemMarketPrices result)
+        {
+            if (FastSoldInFleaPlugin.IsKeyPressed)
+            {
+                FastSoldInFleaPlugin.logSource.LogWarning("DEBUG avg перехват -> " + result.avg);
+                FastSoldInFleaPlugin.LastCatchedPrice = result.avg - 1;
+                FastSoldInFleaPlugin.logSource.LogWarning("DEBUG перехваченный id -> " + FastSoldInFleaPlugin.LastCatchedItemID);
+
+                if (FastSoldInFleaPlugin.LastCachedItem == null || FastSoldInFleaPlugin.LastCatchedItemID == null)
+                {
+                    FastSoldInFleaPlugin.logSource.LogError("DEBUG Что-то пустенькое. Не создаём оффер");
+                    return true;
+                }
+                FastSoldInFleaPlugin.TryAddOfferToFlea(FastSoldInFleaPlugin.LastCachedItem, FastSoldInFleaPlugin.LastCatchedPrice);
+            }
+            return true;
+        }
+    }
+    
+    internal class FleaCatchItemPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod() => typeof(GridItemView).GetMethod("OnClick", BindingFlags.Instance | BindingFlags.Public);
+
+        [PatchPrefix]
+        static bool Prefix(GridItemView __instance)
+        {
+            if (__instance.Item == null)
+            {
+                return true;
+            }
+            FastSoldInFleaPlugin.LastCachedItem = __instance.Item;
+            FastSoldInFleaPlugin.LastCatchedItemID = __instance.Item.TemplateId;
+            FastSoldInFleaPlugin.logSource.LogWarning($"Перехват ITEM ID {__instance.Item.Id}");
+            FastSoldInFleaPlugin.logSource.LogWarning($"Перехват ITEM TemplateId {__instance.Item.TemplateId}");
+            return true;
+        }
+    }
+}
