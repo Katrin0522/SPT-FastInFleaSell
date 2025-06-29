8ff4a6d6d3d4c381d97f1f11c8066e92b08aed36
Kat <egorsimonoff2018@gmail.com>
2025-06-29 03:15:46 +0300

    Replace button with price

--- Patches/FleaCatchPricePatch.cs
+++ Patches/FleaCatchPricePatch.cs
@@ -1,8 +1,11 @@
 ﻿using System.Reflection;
+using EFT.InventoryLogic;
+using EFT.UI;
 using EFT.UI.DragAndDrop;
 using EFT.UI.Ragfair;
 using HarmonyLib;
 using SPT.Reflection.Patching;
+using TMPro;
 
 namespace FastSoldInFlea.Patches
 {
@@ -18,16 +21,17 @@ namespace FastSoldInFlea.Patches
         {
             if (FastSoldInFleaPlugin.IsKeyPressed)
             {
-                FastSoldInFleaPlugin.logSource.LogWarning("DEBUG avg перехват -> " + result.avg);
+                FastSoldInFleaPlugin.logSource.LogWarning("DEBUG avg catch -> " + result.avg);
                 FastSoldInFleaPlugin.LastCatchedPrice = result.avg - 1;
-                FastSoldInFleaPlugin.logSource.LogWarning("DEBUG перехваченный id -> " + FastSoldInFleaPlugin.LastCatchedItemID);
+                FastSoldInFleaPlugin.logSource.LogWarning("DEBUG catch id -> " + FastSoldInFleaPlugin.LastCatchedItemID);
 
                 if (FastSoldInFleaPlugin.LastCachedItem == null || FastSoldInFleaPlugin.LastCatchedItemID == null)
                 {
-                    FastSoldInFleaPlugin.logSource.LogError("DEBUG Что-то пустенькое. Не создаём оффер");
+                    FastSoldInFleaPlugin.logSource.LogError("Data is null. Not create offer");
                     return true;
                 }
                 FastSoldInFleaPlugin.TryAddOfferToFlea(FastSoldInFleaPlugin.LastCachedItem, FastSoldInFleaPlugin.LastCatchedPrice);
+                return false;
             }
             return true;
         }
@@ -46,9 +50,50 @@ namespace FastSoldInFlea.Patches
             }
             FastSoldInFleaPlugin.LastCachedItem = __instance.Item;
             FastSoldInFleaPlugin.LastCatchedItemID = __instance.Item.TemplateId;
-            FastSoldInFleaPlugin.logSource.LogWarning($"Перехват ITEM ID {__instance.Item.Id}");
-            FastSoldInFleaPlugin.logSource.LogWarning($"Перехват ITEM TemplateId {__instance.Item.TemplateId}");
+            FastSoldInFleaPlugin.TryGetPrice(FastSoldInFleaPlugin.LastCachedItem, price =>
+            {
+	            FastSoldInFleaPlugin.logSource.LogWarning($"CATCH ITEM ID {__instance.Item.Id}");
+	            FastSoldInFleaPlugin.logSource.LogWarning($"CATCH ITEM Price {price}");
+	            FastSoldInFleaPlugin.logSource.LogWarning($"CATCH ITEM TemplateId {__instance.Item.TemplateId}");
+            });
+            
             return true;
         }
     }
+    
+    public class ContextMenuAddOfferPatch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return AccessTools.Method(typeof(ContextMenuButton), "Show", null, null);
+		}
+		
+		[PatchPostfix]
+		public static void Postfix(string caption, TextMeshProUGUI ____text)
+		{
+			if (!FastSoldInFleaPlugin.IsKeyPressed)
+			{
+				return;
+			}
+			
+			
+			RagFairClass ragFair = FastSoldInFleaPlugin.Session.RagFair;
+			if (ragFair != null && ragFair.Available)
+			{
+				int myOffersCount = ragFair.MyOffersCount;
+				int maxOffersCount = ragFair.MaxOffersCount;
+				string text = string.Format("AddOfferButton{0}/{1}".Localized(null), myOffersCount, maxOffersCount);
+				if (caption == text)
+				{
+					FastSoldInFleaPlugin.logSource.LogWarning("Update button text");
+					double cachePrice = 0.0;
+					FastSoldInFleaPlugin.TryGetPrice(FastSoldInFleaPlugin.LastCachedItem, price =>
+					{
+						cachePrice = price;
+					});
+					____text.text = $"Sold for {cachePrice}";
+				}
+			}
+		}
+	}
 }
