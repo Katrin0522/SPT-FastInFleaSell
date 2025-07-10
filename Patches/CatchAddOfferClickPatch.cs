88442cbc6fab5d85f7965c52f91dee301677f278
Kat <katrinfox0522@gmail.com>
2025-07-10 22:35:21 +0300

    Add: Support UIFixes with MultiSelect for sell

--- Patches/CatchAddOfferClickPatch.cs
+++ Patches/CatchAddOfferClickPatch.cs
@@ -3,6 +3,7 @@ using EFT.Communications;
 using FastSellInFlea.Models;
 using HarmonyLib;
 using SPT.Reflection.Patching;
+using UIFixesInterop;
 
 namespace FastSellInFlea.Patches
 {
@@ -35,6 +36,41 @@ namespace FastSellInFlea.Patches
                 {
                     return false;
                 }
+
+                if (MultiSelect.Count > 1)
+                {
+                    var myOffersCountNow = FastSellInFleaPlugin.Session.RagFair.MyOffersCount;
+                    var maxOffersCountNow = FastSellInFleaPlugin.Session.RagFair.MaxOffersCount;
+                    int offersAdded = 0;
+                    int offerLimit = maxOffersCountNow - myOffersCountNow;
+                    
+                    foreach (var selectedItem in MultiSelect.Items)
+                    {
+                        if (offersAdded >= offerLimit)
+                            break;
+                        
+                        if (FastSellInFleaPlugin.CanBeSelectedAtRagfair(selectedItem))
+                        {
+                            FastSellInFleaPlugin.TryGetPrice(selectedItem, price =>
+                            {
+                                if (price <= 0 || offersAdded >= offerLimit)
+                                    return;
+                                
+                                FastSellInFleaPlugin.LastCachePrice = price;
+                                offersAdded++;
+                                FastSellInFleaPlugin.TryAddOfferToFlea(selectedItem, price);
+                            });
+                        }
+                    }
+                    
+                    FastSellInFleaPlugin.CachedTextButton = null;
+                    FastSellInFleaPlugin.CachedOriginalText = "";
+                    FastSellInFleaPlugin.CachedNewText = "";
+                    FastSellInFleaPlugin.LastCacheItem = null;
+                    FastSellInFleaPlugin.LastCachePrice = 0.0;
+                    return false;
+                }
+                
                 
                 FastSellInFleaPlugin.TryAddOfferToFlea(FastSellInFleaPlugin.LastCacheItem,
                     FastSellInFleaPlugin.LastCachePrice);
